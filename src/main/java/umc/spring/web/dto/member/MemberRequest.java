9c4bf41e042f33b44a00855886eb0bb309c61189
fly-like-you <qkrwnsgh71w@gmail.com>
2024-01-17 17:18:08 +0900

    9주차 - Service의 validation을 옮기기

--- src/main/java/umc/spring/web/dto/member/MemberRequest.java
+++ src/main/java/umc/spring/web/dto/member/MemberRequest.java
@@ -1,19 +1,36 @@
 package umc.spring.web.dto.member;
 
 import java.util.List;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.Getter;
 import umc.spring.domain.mapping.MemberPrefer;
+import umc.spring.validation.annotation.ExistCategories;
 
 public class MemberRequest {
 
     @Getter
     public static class JoinDTO{
+        @NotBlank
         String name;
+
+        @NotNull
         Integer birthYear;
+
+        @NotNull
         Integer birthMonth;
+
+        @NotNull
         Integer birthDay;
+
+        @Size(min=5, max=12)
         String address;
+
+        @Size(min=5, max=12)
         String specAddress;
+
+        @ExistCategories
         List<Long> preferList;
     }
 }
